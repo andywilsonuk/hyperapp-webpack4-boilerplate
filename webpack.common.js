ef0bc93dc2d1770f3ceded67adce54bd0241e43d
Andy Wilson <wilsona@rsc.org>
2019-03-05 14:47:47 +0000

    Added url-loader and linting command

--- webpack.common.js
+++ webpack.common.js
@@ -10,11 +10,19 @@ module.exports = {
         path: Path.resolve(__dirname, 'dist'),
     },
     module: {
-        rules: [{
-            test: /\.js$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-        }],
+        rules: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader',
+			},
+			{
+				test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+				loader: 'url-loader',
+				options: {
+					limit: 10000
+				}
+			}],
     },
     plugins: [
         new MiniCssExtractPlugin({
