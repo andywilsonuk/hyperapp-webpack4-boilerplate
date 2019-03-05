7719c0575989268db0e6f249ca8dff9464511390
Andy Wilson <wilsona@rsc.org>
2019-03-05 14:59:34 +0000

    Updated to use html-webpack-plugin for cache busting of assets

--- webpack.common.js
+++ webpack.common.js
@@ -1,12 +1,13 @@
 const Path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: {
         main: './src/index.js',
     },
     output: {
-        filename: 'bundle.js',
+        filename: 'bundle.[hash].js',
         path: Path.resolve(__dirname, 'dist'),
     },
     module: {
@@ -26,7 +27,10 @@ module.exports = {
     },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: './bundle.css',
+            filename: './bundle.[hash].css',
         }),
+		new HtmlWebpackPlugin({
+			template: './src/index.html'
+		}),
     ],
 };
