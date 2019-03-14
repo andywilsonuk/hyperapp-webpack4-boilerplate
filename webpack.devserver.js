31049bebdb2064bdfa4ccd57f9985a8fabca5a93
Andy Wilson <wilsona@rsc.org>
2019-03-14 11:22:33 +0000

    Fixed CSS hot load issue on devserver

--- webpack.devserver.js
+++ webpack.devserver.js
@@ -1,8 +1,9 @@
 const Path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const merge = require('webpack-merge');
 const dev = require('./webpack.dev.js');
 
-module.exports = merge(dev, {
+const baseMerge = merge(dev, {
     devServer: {
         contentBase: Path.join(__dirname, 'dist'),
         open: 'Chrome',
@@ -10,3 +11,20 @@ module.exports = merge(dev, {
         publicPath: '/',
     },
 });
+
+// css-hot-loader doesn't seem to work with the hashed filename so use a static name with devserver
+module.exports = merge({
+        customizeArray: merge.unique(
+            'plugins',
+            ['MiniCssExtractPlugin'],
+            plugin => plugin.constructor && plugin.constructor.name,
+        ),
+    })(
+    {
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: './app.css',
+            }),
+        ],
+    }, baseMerge,
+);
